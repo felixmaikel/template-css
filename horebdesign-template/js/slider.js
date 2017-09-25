cab34cba17b31b0590a5f341499ffdb34ce159d4
felixmaikel <felix.maikel@gmail.com>
2017-09-25 22:47:44 +0200

    footer design and slider video

--- horebdesign-template/js/slider.js
+++ horebdesign-template/js/slider.js
@@ -1,4 +1,5 @@
 var index = 0;
+var videoIndex = 0;
 
 function recalculateMargin(position) {
 	return position * -1 * 100;
@@ -29,6 +30,35 @@ function moveSlider() {
 	}
 	showSlider(index);
 	index++;
+}
+
+function showVideoSlider(position) {
+	videoIndex = position;
+	var sliders = document.getElementsByClassName("video-slider");
+
+	for(i=0; i<sliders.length; i++) {
+		if(sliders[i].className.includes("video-slider-active")) {
+			sliders[i].className = sliders[i].className.replace("video-slider-active","");
+			break;
+		}
+	}
+	sliders[position].className += " video-slider-active";
+}
+
+function showVideoSliderNext() {
+	videoIndex++;
+	if(videoIndex > 3) {
+		videoIndex = 0;
+	}
+	showVideoSlider(videoIndex);
+}
+
+function showVideoSliderAfter() {
+	videoIndex--;
+	if(videoIndex < 0) {
+		videoIndex = 3;
+	}
+	showVideoSlider(videoIndex);
 }
 
 setInterval("moveSlider()", 2000);
